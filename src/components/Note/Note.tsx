b32d60d0675f9946ba63f887032ffb32ccbcebed
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-04-01 17:12:11 +0300

    feat: add notes design

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -1,3 +1,19 @@
+import './Note.scss';
+
 export default function Note() {
-  return <div>One note</div>;
+  const arrTags = [];
+  for (let i = 0; i < 9; i += 1) {
+    arrTags.push(<div className="note__tag">Tag</div>);
+  }
+  return (
+    <div className="note">
+      <section className="note__header">{arrTags}</section>
+      <article className="note__text-wrapper">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate
+        odio sequi ducimus, temporibus perspiciatis nesciunt accusantium vel
+        veniam dolorum dolorem alias. Culpa beatae quos porro in eligendi ex
+        maxime deserunt.
+      </article>
+    </div>
+  );
 }
